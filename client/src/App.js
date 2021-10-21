f409be5bdd7dd1cbb2d3d1487ee8eea466eea78c
aditya <aditya.icf@gmail.com>
2021-10-21 16:35:52 +0530

    Bootstrap setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
+import './App.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">Equivalent Salary Converter</a>
+    </nav>
+
+
+    <footer className="footer">
+      <div className="container">
+        <p className="text-muted float-left">Made with ❤️ by <a href="https://adityarajput.com">Aditya Rajput</a></p>
+        <p className="text-muted float-right">
+          <a href="https://www.linkedin.com/in/aditya-kumar-rajput/"><i class="bi bi-linkedin mr-3"></i></a>
+          <a href="https://www.facebook.com/Aditya.Rajput.XMR/"><i class="bi bi-facebook mr-3"></i></a>
+          <a href="https://www.instagram.com/adityaxrajput"><i class="bi bi-instagram mr-3"></i></a>
+          <a href="https://github.com/adixmr/salaryconverter"><i class="bi bi-github"></i></a>
+        </p>
+      </div>
+    </footer>
+    </>
   );
 }
 
