8a383941c3bc94bdc54773029b4eb933b60c24db
aditya <aditya.icf@gmail.com>
2021-10-21 15:24:06 +0530

    react init

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,29 +4,36 @@ const ppp = require('../data/final.json')
 
 
 router.get('/:from/:to', async (req, res) => {
-    let from        = req.params.from;
-    let to          = req.params.to;
-    let conversion  = await axios.get(`https://freecurrencyapi.net/api/v2/latest?apikey=${process.env.CURRENCY_KEY}&base_currency=`+ppp[from]['currency_code'])
-    
-    let result = {
-        country : {
-            from:  ppp[from]['country'],
-            to: ppp[to]['country']
-        },
-        currency : {
-            from: ppp[from]['currency_code'],
-            to: ppp[to]['currency_code']
-        }, 
-        conversion: {
-            direct: conversion.data.data[ppp[to]['currency_code']],
-            reverse: 1/conversion.data.data[ppp[to]['currency_code']],
-            get factor() {
-                return (ppp[to]['PPP']/ppp[from]['PPP'])/this.direct;
-            } 
+    try {
+        let from        = req.params.from;
+        let to          = req.params.to;
+        let conversion  = await axios.get(`https://freecurrencyapi.net/api/v2/latest?apikey=${process.env.CURRENCY_KEY}&base_currency=`+ppp[from]['currency_code'])
+        
+        let result = {
+            country : {
+                from:   ppp[from]['country'],
+                to:     ppp[to]['country']
+            },
+            currency : {
+                from:   ppp[from]['currency_code'],
+                to:     ppp[to]['currency_code']
+            }, 
+            conversion: {
+                direct: conversion.data.data[ppp[to]['currency_code']],
+                get reverse() {
+                    return 1/this.direct;
+                },
+                get factor() {
+                    return (ppp[to]['PPP']/ppp[from]['PPP'])/this.direct;
+                } 
+            }
         }
+        
+        res.json(result);
+    } catch (err) {
+        console.error(err.message);
+        res.json({error: 'There is some error with your request'})
     }
-    
-    res.json(result);
 })
 
 module.exports = router;
